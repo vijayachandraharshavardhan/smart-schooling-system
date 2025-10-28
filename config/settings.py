16053d669355aed81ca427dc747b6b64b38a2dd1
vijayachandraharshavardhan@gmail.com <vijayachandraharshavardhan@gmail.com>
2025-10-29 00:33:21 +0530

    Fix Render environment detection using RENDER_SERVICE_ID

--- config/settings.py
+++ config/settings.py
@@ -184,7 +184,7 @@ SESSION_COOKIE_SAMESITE = "Lax"
 # -----------------------------------
 # PRODUCTION SETTINGS OVERRIDE
 # -----------------------------------
-if os.environ.get('RAILWAY_ENVIRONMENT') or os.environ.get('HEROKU_APP_NAME') or os.environ.get('RENDER'):
+if os.environ.get('RAILWAY_ENVIRONMENT') or os.environ.get('HEROKU_APP_NAME') or os.environ.get('RENDER_SERVICE_ID'):
     from .settings_production import *
 
 # -----------------------------------
