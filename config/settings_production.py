bd4ff7fa90f38c1bbe8b0848627d9a0b61c70b93
vijayachandraharshavardhan@gmail.com <vijayachandraharshavardhan@gmail.com>
2025-10-29 03:19:04 +0530

    Add Cloudinary for logo and media storage

--- config/settings_production.py
+++ config/settings_production.py
@@ -131,3 +131,18 @@ LOGGING = {
 # AUTO SUPERUSER PASSWORD (optional)
 # -----------------------------------
 os.environ.setdefault('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+
+# -----------------------------------
+# MEDIA STORAGE (Cloudinary)
+# -----------------------------------
+if os.environ.get("CLOUDINARY_URL"):
+    INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
+
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    }
+
+MEDIA_URL = '/media/'
